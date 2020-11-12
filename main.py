3244f5d56f5a160e2da0b25ef81ca48b2cbc506d
gkovan <gkovan@hotmail.com>
2020-11-12 00:42:49 -0500

    basics of maze are working like collisions

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ import pygame
 
 
 class Player:
-    x = 44
-    y = 44
+    prev_x = 1
+    prev_y = 1
+
+    x = 1
+    y = 1
     speed = 1
 
     def moveRight(self):
-        self.x = self.x + 44
+        self.x = self.x + 1
 
     def moveLeft(self):
-        self.x = self.x - 44
+        self.x = self.x - 1
 
     def moveUp(self):
-        self.y = self.y - 44
+        self.y = self.y - 1
 
     def moveDown(self):
-        self.y = self.y + 44
+        self.y = self.y + 1
 
 
 class Maze:
@@ -25,15 +28,32 @@ class Maze:
         self.M = 10
         self.N = 8
         self.maze = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                     1, 0, 0, 0, 0, 2, 2, 2, 0, 1,
+                     1, 0, 0, 0, 0, 2, 0, 0, 2, 1,
                      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                     1, 0, 1, 1, 1, 1, 1, 1, 0, 1,
+                     1, 0, 1, 1, 1, 1, 1, 1, 2, 1,
                      1, 0, 1, 0, 0, 0, 0, 0, 0, 1,
                      1, 0, 1, 0, 1, 1, 1, 1, 0, 1,
                      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
                      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ]
 
-    def draw(self, display_surf, image_surf, coin_surf):
+    def draw(self, display_surf, image_surf, coin_surf, player_surf, player):
+
+        # check for collision with the wall
+        if self.maze[player.x + (player.y*self.M)] == 1:
+            # collision with a wall so reset player x and y to previous values
+            player.x = player.prev_x
+            player.y = player.prev_y
+        else:
+            # no collision with wall so set the player previous x and y values to the current
+            player.prev_x = player.x
+            player.prev_y = player.y
+
+        # check for collision with coin
+        if self.maze[player.x + (player.y * self.M)] == 2:
+            print("COLLISION")
+            self.maze[player.x + (player.y * self.M)] = 0
+
+        display_surf.blit(player_surf, (player.x * 44, player.y * 44))
         bx = 0
         by = 0
         for i in range(0, self.M * self.N):
@@ -73,10 +93,10 @@ class App:
         pygame.display.set_caption('Pygame pythonspot.com example')
         self._running = True
         self._coin_surf = pygame.image.load("coin.jpg")
-        self._image_surf = pygame.image.load("player.png")
+        self._player_surf = pygame.image.load("player.png")
         self._block_surf = pygame.image.load("block.png").convert()
         print("GKGKGK")
-        print(self._image_surf.get_rect())
+        print(self._player_surf.get_rect())
 
     def on_event(self, event):
         if event.type == QUIT:
@@ -87,8 +107,8 @@ class App:
 
     def on_render(self):
         self._display_surf.fill((0, 0, 0))
-        self._display_surf.blit(self._image_surf, (self.player.x, self.player.y))
-        self.maze.draw(self._display_surf, self._block_surf, self._coin_surf)
+        #self._display_surf.blit(self._player_surf, (self.player.x, self.player.y))
+        self.maze.draw(self._display_surf, self._block_surf, self._coin_surf, self._player_surf, self.player)
         pygame.display.flip()
 
     def on_cleanup(self):
